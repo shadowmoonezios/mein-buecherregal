9c8a8653b6ff107e89b7bf7a0df99083f5c11c95
shadowmoonezios <agushifelde@hotmail.com>
2025-11-05 13:39:40 +0200

    feat: initial commit for server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(bodyParser.json());
+
+// MongoDB Verbindung
+mongoose.connect('mongodb://localhost:27017/mein-buecherregal', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('MongoDB verbunden');
+}).catch(err => console.log(err));
+
+app.get('/', (req, res) => {
+  res.send('Willkommen bei Mein Bücherregal!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server läuft auf Port ${PORT}`);
+});
